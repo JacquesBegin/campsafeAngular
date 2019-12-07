d763da991c599576dc7c8a500182382ac26a7f72
Jacques Begin <jacques.andre.begin@gmail.com>
2019-12-06 19:55:10 -0500

    Creates component for successful load message.

---
+++ campsafeAngular/src/app/success/success.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success',
+  template: `<p>Successfully Loaded</p>`,
+  styles: ['p {color: #00b300; }']
+})
+
+export class SuccessComponent {
+
+}
