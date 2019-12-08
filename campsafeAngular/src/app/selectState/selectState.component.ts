979e32eac3a1fdb96d563b9e7ebd123ab6491cfa
Jacques Begin <jacques.andre.begin@gmail.com>
2019-12-07 23:24:54 -0500

    Creates initial ts file for selectState component.

---
+++ campsafeAngular/src/app/selectState/selectState.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-select-state',
+  templateUrl: './selectState.component.html',
+  styleUrls: ['./selectState.component.css']
+})
+
+export class SelectState {
+  
+}
