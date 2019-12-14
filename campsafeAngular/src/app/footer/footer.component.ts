03fb833ecae1cea35523f9781b5b63d551defb95
Jacques Begin <jacques.andre.begin@gmail.com>
2019-12-13 19:29:09 -0500

    Adds function to return the current year.

--- campsafeAngular/src/app/footer/footer.component.ts
+++ campsafeAngular/src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+
+  getCurrentYear = () => {
+    let date = new Date();
+    return date.getFullYear();
+  }
+  
 
 }
