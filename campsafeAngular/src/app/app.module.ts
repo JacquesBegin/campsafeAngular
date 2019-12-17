3c4f72e0b5b60622f41c15f5d1437283d7f7a724
Jacques Begin <jacques.andre.begin@gmail.com>
2019-12-16 23:27:07 -0500

    Adds formsmodule to app.module.

--- campsafeAngular/src/app/app.module.ts
+++ campsafeAngular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SuccessComponent } from './success/success.component';
@@ -22,7 +23,8 @@ import { BindingComponent } from './binding/binding.component';
     BindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
