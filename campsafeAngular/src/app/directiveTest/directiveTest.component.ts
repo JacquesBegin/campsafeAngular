ef87b6db18a9c39d5b363a050e81158de1b48850
Jacques Begin <jacques.andre.begin@gmail.com>
2019-12-19 11:25:25 -0500

    Creates comonent for directiveTest.

---
+++ campsafeAngular/src/app/directiveTest/directiveTest.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directive-test',
+  templateUrl: './directiveTest.component.html',
+  styleUrls: ['./directiveTest.component.css']
+})
+
+export class DirectiveTestComponent {
+
+}
