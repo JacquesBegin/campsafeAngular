fe7018decc163c508e1e0fb64aa6b4adca51e169
Jacques Begin <jacques.andre.begin@gmail.com>
2019-12-20 13:59:59 -0500

    Refactors code is ts file.

--- campsafeAngular/src/app/directiveTest/directiveTest.component.ts
+++ campsafeAngular/src/app/directiveTest/directiveTest.component.ts
@@ -8,22 +8,11 @@ import { Component } from '@angular/core';
 
 export class DirectiveTestComponent {
   display: boolean = false;
-  count: number = 0;
   countLog = [];
-  active = false;
-
-  setDisplay() {
-    this.display = this.display ? false : true;
-  }
 
   onClickLogCount() {
-    this.count++;
-    this.countLog.push(this.count);
-    if (this.count >= 5) {
-      this.active = true;
-    }
-    this.setDisplay();
-
+    this.countLog.push(new Date());
+    this.display = !this.display;
   }
 
 }
